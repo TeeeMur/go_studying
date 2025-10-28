3f9347cda379b3ed8d94950fb837a054b0acd0ce
timur <timurs.kzn@gmail.com>
2025-10-28 12:19:17 +0300

    3rd hw ready

--- bashnya-hw3/hw3.go
+++ bashnya-hw3/hw3.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("alskdjfasldj")
+	tree := BST{}
+	tree.Insert(5)
+	tree.Insert(8)
+	tree.Insert(3)
+	tree.Insert(6)
+	eightNode := tree.Find(8)
+	fmt.Println(*eightNode)
+	fmt.Println(eightNode.left.key) // must be 6
 }
